5ac7ae14acc1a1006d7c50656564fd2cd0eaca07
matt keef <mkeef1@gmail.com>
2014-09-18 13:21:26 -0500

    weather updated

--- client/components/directives/mkweather/mkweather.js
+++ client/components/directives/mkweather/mkweather.js
@@ -4,8 +4,8 @@
 
   angular.module('mkWeatherModule', [])
   .factory('WeatherApi', ['$http', function($http){
-    function conditions(zip){
-      return $http.jsonp('http://api.wunderground.com/api/d400acb7c08a6ba9/conditions/q/' + zip + '.json?callback=JSON_CALLBACK');
+    function conditions(query){
+      return $http.jsonp('http://api.wunderground.com/api/d400acb7c08a6ba9/conditions/q/' + query + '.json?callback=JSON_CALLBACK');
     }
 
     return {conditions:conditions};
@@ -19,9 +19,23 @@
     o.link        = function(scope, element, attrs){
                       };
     o.controller  = ['$scope', 'WeatherApi', function($scope, WeatherApi){
-                        WeatherApi.conditions($scope.zip).then(function(response){
-                          $scope.weather = response.data.current_observation.temp_f;
-                      });
+
+                        $scope.$on('position', function(event, pos){
+                          if($scope.zip){return;}
+
+                          console.log('i am weather', pos);
+                          var query = pos.coords.latitude + ',' + pos.coords.longitude;
+                          console.log(query);
+                          weather(query);
+                        });
+
+                        function weather(query){
+                          WeatherApi.conditions(query).then(function(response){
+                            $scope.weather = response.data.current_observation.temp_f;
+                            $scope.icon = response.data.current_observation.icon_url;
+                          });
+                        }
+                        if($scope.zip){weather($scope.zip);}
                     }];
     return o;
   }]);
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -5,7 +5,6 @@
   .controller('HomeCtrl', ['$scope', 'Home', 'Movie', function($scope, Home, Movie){
     $scope.people = [{name:'Bob', age:'25'}, {name:'Matt', age:'30'}];
     $scope.symbols = ['AAPL', 'GOOG', 'AMZN'];
-    $scope.zips = ['37069'];
     $scope.titles = ['Toy Story', 'Rocky', 'Evil Dead'];
 
     $scope.delMovie = function(index){
